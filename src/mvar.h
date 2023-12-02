346186bccc35e71ca1ac6c09e8f1948876d3a7d9
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-02 18:03:58 -0500

    lower allocation sizes to optimize memory usage

--- src/mvar.h
+++ src/mvar.h
@@ -3,16 +3,18 @@
 #ifndef _MVAR_H
 #define _MVAR_H
 
+#include <stdint.h>
+
 #define MERC_VERSION "1.0"
 #define MERC_MEMORY_POOLING_SIZE 8
 #define MERC_HARDLIMIT 1024
-#define MERC_LISTING_INITIAL_SIZE 50
+#define MERC_LISTING_INITIAL_SIZE 25
 
 #define MERC_REGISTER_MAX 256
 #define MERC_SECTION_MAX 256
 #define MERC_REGISTER_BYTE_MAX 256
 #define MERC_SECTION_BYTE_MAX 256
 
-#define byte char
+#define byte uint32_t
 
 #endif /* _MVAR_H */
