7be889304087091da6ae7eadfb86796c13a2ede1
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-02 18:03:26 -0500

    add byte shifting

--- src/msyscalls.c
+++ src/msyscalls.c
@@ -11,3 +11,11 @@ merc_sys_write (MReg *regist, MCpu *cpu)
 
   return 0;
 }
+
+/*shift data(0) left by data(1)*/
+int
+merc_sys_shift0 (MReg *regist, MCpu *cpu)
+{
+  regist->data[0] <<= regist->data[1];
+  return 0;
+}
