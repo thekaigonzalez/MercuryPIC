c5b1bd2221edd1f0211c684fd74b771775fdc27b
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-02 14:18:57 -0500

    add system calls

--- src/mcpu.c
+++ src/mcpu.c
@@ -17,6 +17,11 @@ MCpuInitialize (MCpu *cpu)
   cpu->op_count = 0;
   cpu->op_capacity = MERC_MEMORY_POOLING_SIZE;
   cpu->blk = root;
+
+  cpu->systemcall
+      = MPoolAlloc (root, MERC_MEMORY_POOLING_SIZE * sizeof (MSysCall));
+  cpu->systemcall_count = 0;
+  cpu->systemcall_capacity = MERC_MEMORY_POOLING_SIZE;
 }
 
 void
@@ -74,6 +79,75 @@ MRegister (MCpu *cpu, int position)
   if (cpu && position >= 0 && position < MERC_REGISTER_MAX)
     {
       return &cpu->registers[position];
+    }
+
+  return NULL;
+}
+
+MSysCall *
+MSysCallFind (MCpu *cpu, byte id)
+{
+  if (cpu)
+    {
+      for (int i = 0; i < cpu->systemcall_count; i++)
+        {
+          if (cpu->systemcall[i] && cpu->systemcall[i]->id == id)
+            {
+              return cpu->systemcall[i];
+            }
+        }
+    }
+  return NULL;
+}
+
+void
+MAddSyscall (MCpu *cpu, byte id, int (*function) (MReg *reg, MCpu *cpu))
+{
+  if (cpu)
+    {
+      if (cpu->systemcall_count == cpu->systemcall_capacity)
+        {
+          cpu->systemcall_capacity += MERC_MEMORY_POOLING_SIZE;
+          cpu->systemcall
+              = MPoolRealloc (cpu->blk, cpu->systemcall,
+                              cpu->systemcall_capacity * sizeof (MSysCall));
+        }
+
+      cpu->systemcall[cpu->systemcall_count]
+          = MSysCallCreate (cpu->blk, id, function);
+
+      cpu->systemcall_count++;
+    }
+}
+
+MSysCall *
+MSysCallCreate (MMemoryPool *pool, byte id,
+                int (*function) (MReg *reg, MCpu *cpu))
+{
+  MSysCall *call = MPoolAlloc (pool, sizeof (MSysCall));
+
+  call->id = id;
+  call->function = function;
+
+  return call;
+}
+
+byte
+MSysCallRetId (MSysCall *call)
+{
+  if (call)
+    {
+      return call->id;
+    }
+
+  return 0;
+}
+
+int (*MSysCallRetFunction (MSysCall *call)) (MReg *reg, MCpu *cpu)
+{
+  if (call)
+    {
+      return call->function;
     }
 
   return NULL;
